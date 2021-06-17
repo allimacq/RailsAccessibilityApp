17a4c07eb6df3633bfbd75c9f230df1ad7a1c2ee
Allison <allimacq@gmail.com>
2021-06-16 20:55:41 -0400

    add parks edit update and show actions with respective views

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -15,6 +15,34 @@ class ParksController < ApplicationController
         end
     end
 
+    def show
+        @park = Park.find_by_id(params[:id])
+        if !params[:accessible].blank?
+            @park_reviews = @park.accessible_reviews(params[:accessible])
+                if @park_reviews.empty?
+                    flash.now[:alert] = "Could not find any reviews matching your search criteria"
+                end 
+        else
+            @park_reviews = @park.reviews
+        end
+    end
+
+    def edit
+        @park = Park.find(params[:id])
+    end
+
+    def update
+        @park = Park.find(params[:id])
+        @park.update(parks_params)
+        redirect_to park_path(@park)
+    end
+
+    def destroy
+        @park = Park.find(params[:id]).destroy
+        redirect_to root_path, notice: "Successfully deleted."
+    end
+
+
     private
 
     def parks_params
