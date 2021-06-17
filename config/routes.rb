93d18ab2aabcaf6cce73c64f781c3f746a7aed20
Allison <allimacq@gmail.com>
2021-06-17 12:06:24 -0400

    configure google_oauth2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
   resources :states, only: [:index, :show]
   resources :parks
   resources :parks, only: [:show] do
