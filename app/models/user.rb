b6963e94366419760a2bb0be8db8d855b2ac0b5c
Allison <allimacq@gmail.com>
2021-06-16 20:57:22 -0400

    add user associations and #make_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
+  has_many :parks
+  has_many :activities
+  has_many :reviews
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def make_name
+    self.email.split("@")[0].capitalize
+  end
+  
 end
