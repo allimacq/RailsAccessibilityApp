b5040c08a9bffa2c08a4f3330942b10f04bf4c36
Allison <allimacq@gmail.com>
2021-06-16 20:40:21 -0400

    add park validations and #accessible_reviews

--- app/models/park.rb
+++ app/models/park.rb
@@ -3,4 +3,10 @@ class Park < ApplicationRecord
     has_many :reviews, through: :activities
     belongs_to :city
     belongs_to :state
+
+    validates :name, presence: true, uniqueness: true
+
+    def accessible_reviews(accessible)
+        park_reviews = self.reviews.select_by_accessibility(accessible)
+    end
 end
