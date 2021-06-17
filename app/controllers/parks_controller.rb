4ae165b2b956bdbefc2480b885966ee9a0ececaa
Allison <allimacq@gmail.com>
2021-06-16 20:38:10 -0400

    create states#show, parks#new,create along with views, add errors partial for parks view

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,3 +1,24 @@
 class ParksController < ApplicationController
     
+    def new
+        @park = Park.new
+        @state = cookies[:state_id]
+    end
+
+    def create
+        @park = Park.create(parks_params)
+        if @park.save
+            redirect_to park_path(@park)
+        else
+            @state = cookies[:state_id]
+            render :new
+        end
+    end
+
+    private
+
+    def parks_params
+        params.require(:park).permit(:name, :state_id, :city_id, :user_id, :accessible, :rating, :park_reviews)
+    end
+
 end
--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -2,4 +2,9 @@ class StatesController < ApplicationController
     def index
       @states = State.all
     end
+
+    def show
+      cookies.delete :state
+      @state = State.find(params[:id])
+      cookies[:state_id] = @state.id
   end
