8edb2fa805bf86d0a72957737c51488e97e5fde2
Allison <allimacq@gmail.com>
2021-06-16 20:50:21 -0400

    create reviews actions, views, validations, and instance methods

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,48 @@
 class ReviewsController < ApplicationController
+
+    def new
+        @park = Park.find_by_id(params[:park_id])
+        @review = Review.new(park_id: @park.id)
+    end
+
+    def create
+        @review = Review.create(reviews_params)
+        @park = Park.find_by_id(params[:park_id])
+        if @review.save
+            redirect_to park_review_path(@park, @review)
+        else
+            render partial: 'reviews/form'
+        end
+    end
+
+    def show
+        @review = Review.find_by_id(params[:id])
+        @user = User.find_by_id(@review.user_id)
+        @activity = Activity.find_by_id(@review.activity_id)
+        @park = Park.find_by_id(params[:park_id])
+    end
+
+    def edit
+        @park = Park.find(params[:park_id])
+        @review = Review.find(params[:id])
+    end
+
+    def update
+        @review = Review.find(params[:id])
+        @park = Park.find(params[:park_id])
+        @review.update(reviews_params)
+        redirect_to park_review_path(@park, @review)
+    end
+
+    def destroy
+        @review = Review.find(params[:id]).destroy
+        redirect_to root_path, notice: "Successfully deleted."
+    end
+
+
+    private
+
+    def reviews_params
+        params.require(:review).permit(:review, :park_id, :activity_id, :user_id, :accessible, :title, :rating)
+    end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,27 @@
 class Review < ApplicationRecord
     belongs_to :user
     belongs_to :activity
+    belongs_to :park
+
+    validates :review, :rating, :title, presence: true
+    validates :rating, inclusion: { in: 1..5 }
+    validates :user_id, uniqueness: {scope: :park_id, 
+        message: "can only review a park once" }, on: :create
+    scope :select_by_accessibility, -> (selection) { where("accessible = ?", selection) }
+
+    def display_accessibility(user, activity, park)
+        if self.accessible == "true"
+            "#{user.make_name} Found #{activity.name} at #{park.name} To Be Accessible"
+        else
+            "#{user.make_name} Found #{activity.name} at #{park.name} To Not Be Accessible"
+        end
+    end
+
+    def stars
+        if self.rating == 1
+            "#{self.rating} Star"
+        else
+            "#{self.rating} Stars"
+        end
+    end
 end
